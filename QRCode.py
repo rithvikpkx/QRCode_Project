59f9d55b8102d170e23194ff5ef2b39af72259df
Rithvik Praveen Kumar <praveenkumrpk2007@gmail.com>
2025-06-05 00:25:30 -0400

    added ideas and tested qr code generation

--- QRCode.py
+++ QRCode.py
@@ -1,3 +1,12 @@
+#This is the non web app version that I made just to familiarize myself with using segno and figuring out all of the functionality I might need
+
 import segno
 
-segno.make_qr("Hello World")
+#Important variables
+content = "" # The actual content of the QR Code
+output_format = ".png" # The file format of the QR code once it's exported
+border_size = 0 # The amount of border on the QR code
+scale = 1 # Scale factor of qr code --> how many pixels represent 1 pixel
+
+qrcode = segno.make_qr("Hello World")
+qrcode.save("qrcodes/hellowrldqrcode.png")
