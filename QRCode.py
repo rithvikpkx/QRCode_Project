a87f7460f1780b22e1d0642dc8a51fdbc220ed8c
Rithvik Praveen Kumar <praveenkumrpk2007@gmail.com>
2025-06-05 14:21:29 -0400

    finished general qrcode functionality

--- QRCode.py
+++ QRCode.py
@@ -1,12 +1,11 @@
-#This is the non web app version that I made just to familiarize myself with using segno and figuring out all of the functionality I might need
-
 import segno
 
 #Important variables
 content = "" # The actual content of the QR Code
 output_format = ".png" # The file format of the QR code once it's exported
-border_size = 0 # The amount of border on the QR code
+border = 0 # The amount of border on the QR code
 scale = 1 # Scale factor of qr code --> how many pixels represent 1 pixel
+id_num = 0 # Id number used in filename to save the qrcode
 
-qrcode = segno.make_qr("Hello World")
-qrcode.save("qrcodes/hellowrldqrcode.png")
+qrcode = segno.make_qr(content)
+qrcode.save("qrcodes/qrcode" + str(id_num) + output_format, border = border, scale = scale)
